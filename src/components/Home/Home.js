bb3783344348884ebd2f1b7f5ed4f177d71e7ded
DaisyChepyegon <daisychepyegon@gmail.com>
2022-07-26 14:20:34 +0300

    add image to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,17 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      
+    <div className='Home'>
+      <img src='./Homeimages/Home.jpg' />
+      <div className='Homeimage'>
+        <img src='./Homeimages/Home1.jpg' />
+        <div className='text'>
+
+        </div>
+        <img src='./Homeimages/Home1.jpg' />
+      </div>
+
+    </div>
     </div>
   )
 }
