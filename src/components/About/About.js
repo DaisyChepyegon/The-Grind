cadeb018185913a2319ad8d659706cd5bd2e5609
DaisyChepyegon <daisychepyegon@gmail.com>
2022-07-27 14:36:41 +0300

    add unordered list

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,29 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './About.css'
 import about0 from './Aboutimages/joanna-kosinska-B43a-FPxYqU-unsplash.jpg'
 
 function About() {
+  const [showTime, setShowTime] = useState(false)
+
+  function handleClick(){
+    setShowTime(!showTime)
+  }
+
   return (
+    
     <div className='about'>
+    
       <div className='left'>
       <h1>About US</h1>
-      <img src={about0} alt='About0' height={450} width={500} />
+      <img src={about0} alt='About0' height={550} width={500} />
       </div>
       <div className='right'>
+      <h2>The Grind</h2>
       <p >
-        The Grind Coffee House offers a unique coffee house environment.<br />
-        We are not only a place to drop in and get your morning cup of coffee<br />
-        (although you are more than welcome to do that),<br />
-        we are a place where you can sit down and enjoy that tailor-made cup of coffee.<br /> 
-        If you need to work, we have a seating area created specifically for you. <br />
-        If you need to rest, we have a soft-seating area in front of a stone fire place that is perfect for your weary mind and body. <br />
-        We offer a delicious variety of coffee from Safehouse Coffee made by our professionally trained baristas. <br />
-        We have everything from classic coffee to our house made specialty beverages. <br />
-        All of our sauces & syrups are made in-house with all natural ingredients ensuring you the highest quality in taste & health. <br />
-        You can complete your coffee with one of our delicious sweet treats made by our very own baker. <br />
-        We look forward to serving you at The Grind Coffee House!<br />
+        The Grind Coffee House offers a unique coffee house environment.
+        We are not only a place to drop in and get your morning cup of coffee
+        (although you are more than welcome to do that),
+        we are a place where you can sit down and enjoy that tailor-made cup of coffee.
+        If you need to work, we have a seating area created specifically for you.
+        If you need to rest, we have a soft-seating area in front of a stone fire place that is perfect for your weary mind and body.
+        We offer a delicious variety of coffee from Safehouse Coffee made by our professionally trained baristas. 
+        We have everything from classic coffee to our house made specialty beverages. 
+        All of our sauces & syrups are made in-house with all natural ingredients ensuring you the highest quality in taste & health.
+        You can complete your coffee with one of our delicious sweet treats made by our very own baker.
+        We look forward to serving you at The Grind Coffee House!
       </p>
-      <button>Read More</button>
+      <button onClick={handleClick}>Read More</button>
+     
+
+      <h1>Store Hours</h1>
+      <ul>
+        <li>Monday - Thursday 7am - 9pm</li>
+        <li>Friday 7am - 10pm</li>
+        <li>Saturday 8am - 10pm</li>
+      </ul>
+
       </div>
     </div>
   )
