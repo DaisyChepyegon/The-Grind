07b2c84ecf176438d64da8039dc7d30d7c4e2818
DaisyChepyegon <daisychepyegon@gmail.com>
2022-07-28 12:44:30 +0300

    add image galaries

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -13,10 +13,16 @@ import slide10 from './Menuimages/slide10.jpg'
 import slide11 from './Menuimages/slide11.jpg'
 import slide12 from './Menuimages/slide12.jpg'
 import slide13 from './Menuimages/slide13.jpg'
+import slides1 from './Menuimages/slides1.jpg'
+import slides2 from './Menuimages/slides2.jpg'
+import slides3 from './Menuimages/slides3.jpg'
+import slides4 from './Menuimages/slides4.jpg'
+import pastry1 from './Menuimages/pastry1.jpg'
+import pastry2 from './Menuimages/pastry2.jpg'
 
 function Menu() {
   return (
-    <div>
+    <div className='menus'>
 
     <div className='menu'>
     <div className='ourmenu'>
@@ -24,22 +30,39 @@ function Menu() {
       <tr>Coffee</tr>
       <tr>None-Coffee Drinks</tr>
     </div>
+    <div className='best'>
     <h1>Our Bestsellers</h1>
     <div className='bestseller'>
       
       <div className='coffee'>
       <h2>Coffee</h2>
+      <img src={slides4} alt="image0" height={200} width={200}/>
+      <h3> Cappuccino</h3>
+      <p>A double shot of espresso, 2 ounces milk, 2 ounces foam. </p>
+      <img src={slides2} alt="image0" height={200} width={200}/>
+      <h3> Latte</h3>
+      <p> A Double shot of espresso, with 6 ounces of perfectly steamed milk.</p>
 
       </div>
       <div className='pastries'>
       <h2>Pastries</h2>
 
+      <img src={pastry2} alt="image0" height={200} width={200}/>
+      <h3> Blueberry Muffin</h3>
+      <p> Loaded with blueberries, our blueberry muffin is made using fresh blueberries and topped with a brown sugar pecan crumble</p>
+
+      <img src={pastry1} alt="image0" height={200} width={200}/>
+      <h3>Strawberry Scone </h3>
+      <p> Our mouthwatering homemade scone filled with strawberries and topped with a strawberry creme drizzle</p>
+
       </div>
     </div>
+    </div>
 
     </div>
 
-<h1>Galary</h1>
+
+<h1>Picture Collections</h1>
 <div className='slider'>
   
     <img src={slide1} alt="image0" height={200} width={200}/>
@@ -55,6 +78,10 @@ function Menu() {
     <img src={slide11} alt="image0" height={200} width={200}/>
     <img src={slide12} alt="image0" height={200} width={200}/>
     <img src={slide13} alt="image0" height={200} width={200}/>
+    <img src={slides1} alt="image0" height={200} width={200}/>
+    <img src={slides2} alt="image0" height={200} width={200}/>
+    <img src={slides3} alt="image0" height={200} width={200}/>
+    <img src={slides4} alt="image0" height={200} width={200}/>
     </div>
       
     </div>
