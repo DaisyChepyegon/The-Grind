c255460bb29d0590ff559f268fc2545740702b55
DaisyChepyegon <daisychepyegon@gmail.com>
2022-07-28 14:24:27 +0300

    create states for user, password and error message

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,6 +8,15 @@ function Login() {
   const [user, setUser] =useState('');
   const [password, setPassword] =useState('');
   const [errMsg, setErrMsg] =useState('');
+  const [success, setSuccess] =useState(false)
+
+  useEffect(() =>{
+    userRef.current.focus();
+  },[])
+
+  useEffect(() =>{
+    setErrMsg('');
+  },[user, password])
 
   return (
     <div>
