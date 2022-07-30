1bf9aca7aa7c0638e48545c72e79f61072bf0848
DaisyChepyegon <daisychepyegon@gmail.com>
2022-07-31 01:17:53 +0300

    get data using fetch

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,52 +1,55 @@
+import React,{useState, useEffect} from "react";
 import axios from "axios";
-import React,{ Component } from "react";
 import "./Login.css"
 import home from "./Home.jpg"
 
 
-export default class Login extends Component {
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    const data ={
-      email: this.email,
-      password: this.password,
-    }
-    axios.post("http://localhost:8000/register", data)
-    .then(resp => {
-      console.log(resp)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+ function Login () {
+  const [email, setEmail] =useState("")
+  const [password, setPassword] =useState("")
+  
+  function handleSubmit(){
+    let item={email,password}
+    console.log(item)
+ 
   }
 
-  render(){
+  const fetchData = () =>{
+    axios.get("http://localhost:3000/users")
+    .then((resp => {
+      setEmail(resp.item)
+      console.log(resp.item)
+    }))
+  }
+
+  useEffect(() => {
+    fetchData()
+  },[])
 
   return (
     <div className="lgn">
 
       <div className="img">
-        <img src={home} alt="image0" height={550} width={500}/>
+        <img src={home} alt="display" height={550} width={500}/>
       </div>
       
       <div className="login">
       <h1>Login</h1>
-       <form onSubmit={this.handleSubmit} className="log">
+       <form className="log">
         <label>Email </label>
         <input type="email" placeholder="Email"
-          onChange={e => this.email = e.target.value}
+        onChange={e => setEmail( e.target.value)}
         />
         <label>Password</label>
         <input type="password" placeholder="Password"
-          onChange={e => this.password = e.target.value}
+        onChange={e => setPassword( e.target.value)}
          />
       
-        <button>Login</button>
+        <button onClick={handleSubmit}>Login</button>
         </form>
       </div>
     </div>
   );
-  }
+  
 }
+export default Login
